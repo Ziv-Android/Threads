625f52dceb7c6bb6d416332b183a7fffe4d041c4
zhaopeng <Ziv_Android@Outlook.com>
2017-06-27 14:42:22 +0800

    sem demo

--- app/src/main/jni/threads.cpp
+++ app/src/main/jni/threads.cpp
@@ -4,7 +4,17 @@
 
 #include <stdio.h>
 #include <unistd.h>
+
 #include <pthread.h>
+
+//#define MUTEX
+#define SEM
+
+#ifdef SEM
+// sem header
+#include <semaphore.h>
+
+#endif // SEM
 
 #define EXCEPTION_CLASS "java/lang/RuntimeException"
 
@@ -21,8 +31,14 @@ static jmethodID nativeMessageMethod = NULL;
 static JavaVM *gVm = NULL;
 // Java对象的全局引用
 static jobject gObj = NULL;
+
+#ifdef MUTEX
 // 互斥实例
 static pthread_mutex_t mutex;
+#endif // MUTEX
+#ifdef SEM
+static sem_t sem;
+#endif // SEM
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     // 缓存Java虚拟机
@@ -38,12 +54,26 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
  */
 JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_nativeInit
         (JNIEnv *env, jobject obj) {
+#ifdef MUTEX
     // 初始化互斥
     if (0 != pthread_mutex_init(&mutex, NULL)) {
         // 抛出异常
         int result = throwNewException(env, "Unable to initialize mutex.");
         return;
     }
+#endif // MUTEX
+#ifdef SEM
+    /**
+     * sem为指向信号量结构的一个指针；
+     * pshared不为0时此信号量在进程间共享，否则只能为当前进程的所有线程共享；
+     * value给出了信号量的初始值。
+     *
+     * sem_init(sem_t* sem, int pshared, unsigned int value);
+     */
+    if (0 != sem_init(&sem, 0, 0)) {
+        int result = throwNewException(env, "Unable to initialize sem.");
+    }
+#endif // SEM
     if (NULL == gObj) {
         gObj = env->NewGlobalRef(obj);
         if (NULL == gObj) {
@@ -75,10 +105,16 @@ JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_nativeFree
         env->DeleteGlobalRef(gObj);
         gObj = NULL;
     }
-
+#ifdef MUTEX
     if (0 != pthread_mutex_destroy(&mutex)) {
         int result = throwNewException(env, "Unable to destroy mutex.");
     }
+#endif // MUTEX
+#ifdef SEM
+    if (0 != sem_destroy(&sem)) {
+        int result = throwNewException(env, "Unable to destroy sem.");
+    }
+#endif // SEM
 }
 
 /*
@@ -88,11 +124,17 @@ JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_nativeFree
  */
 JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_nativeWorker
         (JNIEnv *env, jobject obj, jint id, jint iterations) {
+#ifdef MUTEX
     if (0 != pthread_mutex_lock(&mutex)) {
         int result = throwNewException(env, "Unable to lock mutex.");
         return;
     }
-
+#endif // MUTEX
+#ifdef SEM
+    if (0 != sem_wait(&sem)) {
+        int result = throwNewException(env, "Ubable to lock sem.");
+    }
+#endif // SEM
     for (int i = 0; i < iterations; ++i) {
         // 准备消息
         char message[26];
@@ -107,10 +149,16 @@ JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_nativeWorker
         // 睡眠1秒
         sleep(1);
     }
-
+#ifdef MUTEX
     if (0 != pthread_mutex_unlock(&mutex)) {
         int result = throwNewException(env, "Unable to unlock mutex.");
     }
+#endif // MUTEX
+#ifdef SEM
+    if (0 != sem_post(&sem)) {
+        int result = throwNewException(env, "Unable to unlock sem.");
+    }
+#endif // SEM
 }
 
 /**
@@ -146,14 +194,15 @@ static void *nativeWorkerThread(void *args) {
 JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_posixThreads
         (JNIEnv *env, jobject obj, jint threads, jint iterations) {
     // 线程句柄
-    pthread_t* handles = new pthread_t[threads];
+    pthread_t *handles = new pthread_t[threads];
     for (int i = 0; i < threads; ++i) {
         NativeWorkerArgs *nativeWorkerArgs = new NativeWorkerArgs();
         nativeWorkerArgs->id = i;
         nativeWorkerArgs->iternation = iterations;
 
         // 创建新线程
-        int result = pthread_create(&handles[i], NULL, nativeWorkerThread, (void *) nativeWorkerArgs);
+        int result = pthread_create(&handles[i], NULL, nativeWorkerThread,
+                                    (void *) nativeWorkerArgs);
 
         if (0 != result) {
             // 抛出异常
@@ -164,7 +213,7 @@ JNIEXPORT void JNICALL Java_com_ziv_threads_MainActivity_posixThreads
 
     // 等待线程结束
     for (int i = 0; i < threads; ++i) {
-        void* result = NULL;
+        void *result = NULL;
         // 连接每个线程句柄
         if (0 != pthread_join(handles[i], &result)) {
             // 抛出异常
